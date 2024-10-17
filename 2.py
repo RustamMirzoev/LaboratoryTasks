c7abdba5ffe7effa6e08ccba146205c7ed377fe9
RustamMirzoev <rustamzoev@gmail.com>
2024-10-17 17:32:49 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,11 @@
+import networkx as nx
+number_of_nodes = 7
+G = nx.path_graph(number_of_nodes)
+G.add_edge(2, 4)
+G.add_edge(3, 1)
+G.add_edge(3, 5)
+G.add_edge(4, 5)
+G.add_edge(5, 6)
+centrality = nx.eigenvector_centrality_numpy(G)
+for n in centrality:
+    print(f'c({n}) = {centrality[n]:.4f}')
